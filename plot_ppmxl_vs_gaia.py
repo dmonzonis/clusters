924bcb9a0c6eef5ee2b8455446e11cc6655a44c9
monzo94 <dmonzonis@protonmail.com>
2018-12-22 21:39:43 +0100

    Use mas/yr in proper motion plots and arrange them vertically

--- plot_ppmxl_vs_gaia.py
+++ plot_ppmxl_vs_gaia.py
@@ -45,32 +45,32 @@ def plot_positions(ppxml_data, gaia_data):
 
 
 def plot_motions(ppxml_data, gaia_data):
-    plt.figure(0, figsize=(12, 6))
+    plt.figure(0, figsize=(6, 12))
     plt.clf()
     plt.title('PPMXL vs Gaia DR2 proper motions')
 
     # PPMXL
-    plt.subplot(121)
-    plt.plot(ppxml_data['pmRA'], ppxml_data['pmDE'], 'k.', markersize=4)
-    plt.errorbar(ppxml_data['pmRA'], ppxml_data['pmDE'],
-                 xerr=ppxml_data['e_pmRA'], yerr=ppxml_data['e_pmDE'],
+    plt.subplot(211)
+    # In PPMXL, pmra and pmdec is given in deg/yr, so convert it to mas/yr
+    plt.plot(ppxml_data['pmRA'] * DEG_TO_MAS, ppxml_data['pmDE'] * DEG_TO_MAS, 'k.', markersize=4)
+    plt.errorbar(ppxml_data['pmRA'] * DEG_TO_MAS, ppxml_data['pmDE'] * DEG_TO_MAS,
+                 xerr=ppxml_data['e_pmRA'] * DEG_TO_MAS, yerr=ppxml_data['e_pmDE'] * DEG_TO_MAS,
                  linestyle='', color='lightblue', marker='', zorder=0)
-    plt.xlabel('pmra (deg/yr)')
-    plt.ylabel('pmdec (deg/yr)')
-    plt.xlim(-0.00001, 0.00001)
-    plt.ylim(-0.00001, 0.00001)
+    plt.xlabel('pmra (mas/yr)')
+    plt.ylabel('pmdec (mas/yr)')
+    plt.xlim(-50, 50)
+    plt.ylim(-50, 50)
 
     # Gaia DR2
-    plt.subplot(122)
-    # In Gaia DR2, pmra and pmdec is given in mas/yr, so convert it to deg/yr
-    plt.plot(gaia_data[4] / DEG_TO_MAS, gaia_data[6] / DEG_TO_MAS, 'k.', markersize=4)
-    plt.errorbar(gaia_data[4] / DEG_TO_MAS, gaia_data[6] / DEG_TO_MAS,
-                 xerr=gaia_data[5] / DEG_TO_MAS, yerr=gaia_data[7] / DEG_TO_MAS,
+    plt.subplot(212)
+    plt.plot(gaia_data[4], gaia_data[6], 'k.', markersize=4)
+    plt.errorbar(gaia_data[4], gaia_data[6],
+                 xerr=gaia_data[5], yerr=gaia_data[7],
                  linestyle='', color='lightblue', marker='', zorder=0)
-    plt.xlabel('pmra (deg/yr)')
-    plt.ylabel('pmdec (deg/yr)')
-    plt.xlim(-0.00001, 0.00001)
-    plt.ylim(-0.00001, 0.00001)
+    plt.xlabel('pmra (mas/yr)')
+    plt.ylabel('pmdec (mas/yr)')
+    plt.xlim(-50, 50)
+    plt.ylim(-50, 50)
 
     plt.tight_layout()
 
